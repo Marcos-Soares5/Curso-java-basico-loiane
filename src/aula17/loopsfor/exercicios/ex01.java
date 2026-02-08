e1eb4b8e49ad11dc4c857bb26e6f270f460e94da
Marcos-soares5 <marcos.antonioss755@gmail.com>
2026-02-08 14:56:44 -0300

    teste

--- src/aula17/loopsfor/exercicios/ex01.java
+++ src/aula17/loopsfor/exercicios/ex01.java
@@ -1,9 +1,24 @@
 package aula17.loopsfor.exercicios;
 
+import java.util.Scanner;
+
 public class ex01 {
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
+        System.out.println("Informe uma nota entre 0 e 10: ");
+        int num = s.nextInt();
+
+        while (num < 0 || num > 10) {
+            System.out.println("Numero invalido! informe um numero valido: ");
+            num = s.nextInt();
+        }
+
+        System.out.println("Número valido! ");
+
+
+        System.out.println("teste");
     }
 }
 
