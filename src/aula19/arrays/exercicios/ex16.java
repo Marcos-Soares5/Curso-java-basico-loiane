ded92705e654297b6a8f38842f4c11a804e648fb
Marcos-soares5 <marcos.antonioss755@gmail.com>
2026-02-15 15:14:50 -0300

    teste 4 gitkraken

--- src/aula19/arrays/exercicios/ex16.java
+++ src/aula19/arrays/exercicios/ex16.java
@@ -28,11 +28,6 @@ public class ex16 {
             }
         }
 
-
-        System.out.println("teste");
-        System.out.println("teste2");
-        System.out.println("teste3");
-
         System.out.println("A soma de elementos armazenados neste vetor que são inferiores a 15 é: " + soma);
         System.out.println("A quantidade de elementos armazenados no vetor que são iguais a 15 é: " + quantiQuinze);
         System.out.println("A média dos elementos armazenados no vetor que são superiores a 15 é: " + ((double)somaMaiorQuinze/quantiMaiorQuinze));
